6098ca064b547c44260d48b6cfa41ce25e1c03f3
Gevorah <jhonsebastianijajiortiz@gmail.com>
2021-04-13 20:28:42 -0500

    Company and Sale base

---
+++ product-prediction/Model/Company.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace product_prediction.Model
+{
+    class Company
+    {
+        private List<Sale> sales; 
+
+        public Company()
+        {
+            sales = new List<Sale>();
+        }
+    }
+}
--- product-prediction/Model/Sale.cs
+++ product-prediction/Model/Sale.cs
@@ -6,5 +6,27 @@ namespace product_prediction.Model
 {
     class Sale
     {
+        public string id { get; }
+        public char branch { get; }
+        public string city { get; }
+        public string customerType { get; }
+        public string gender { get; }
+        public string productLine { get; }
+        public double unitPrice { get; }
+        public int quantity { get; }
+        public double tax { get; }
+        public double total { get; }
+        public DateTime dateTime { get; }
+        public string payment { get; }
+        public double cogs { get; }
+        public double grossMargin { get; }
+        public double grossIncome { get; }
+        public double rating { get; }
+
+        public Sale()
+        {
+
+        }
+
     }
 }
