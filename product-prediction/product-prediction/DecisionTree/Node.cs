fe5094a97c5ce489df3d40e9f2a2cf70f285e877
alejandroarce03 <54712283+alejandroarce03@users.noreply.github.com>
2021-05-14 19:36:11 -0500

    Basic classes of the decision tree

---
+++ product-prediction/product-prediction/DecisionTree/Node.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace product_prediction.DecisionTree
+{
+    class Node<T>
+    {
+        public bool isLeaf { get; set; }
+        public bool isRoot { get; set; }
+        public T value { get; set; }
+
+    }
+}
---
+++ product-prediction/product-prediction/DecisionTree/Tree.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace product_prediction.DecisionTree
+{
+    class Tree<T>
+    {
+
+    }
+}
