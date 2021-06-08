1aa8297c7cf09067d0277da0a4d45326d418877b
alejandroarce03 <54712283+alejandroarce03@users.noreply.github.com>
2021-06-07 21:20:12 -0500

    Experiment Class

---
+++ product-prediction/product-prediction/Experiment/PPExperiment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace product_prediction.Experiment
+{
+    class PPExperiemnt
+    {
+
+        public string MakePrediction(int predictions, string treeType, string[,] inputs)
+        {
+
+            return "time"; 
+        }
+    }
+}
